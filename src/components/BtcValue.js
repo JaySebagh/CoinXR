b61083028fa9bda6e347e5017d3a8b6b8cf789cd
Jay <Jay@Jays-MacBook-Pro.local>
2021-12-02 21:06:17 -0800

    created user currency input field and conversion to coin value

--- src/components/BtcValue.js
+++ src/components/BtcValue.js
@@ -2,21 +2,35 @@ import axios from 'axios';
 import React, { useState, useEffect }  from 'react';
 
 const BtcValue = () => {
-    const [value, setValue] = useState([]);
-  
+    const [data, setData] = useState([]);
+    const [input, setInput] = useState("");
+
     useEffect(() => {
-      const getValue = async () => {
-        const value = await axios.get("https://api.coinbase.com/v2/prices/spot?currency=USD");
-        setValue(value.data.data);
+      const getData = async () => {
+        const data = await axios.get("https://api.coinbase.com/v2/prices/spot?currency=USD");
+        setData(data.data.data);
       };
   
-      getValue();
+      getData();
     }, []);
-  
-    console.log(value)
+
+    const convert= (quantity, value) => {
+        const formula = quantity/value
+        return formula
+    }
+
+    console.log(input)
     return (
         <div>
-            <p>1 {value.base} = {value.amount} {value.currency}</p>
+            <input
+                type="text"
+                min="0"
+                // prevent "e" from being pressed
+                onKeyDown={ (evt) => evt.key === 'e' && evt.preventDefault() }
+                placeholder={data.currency}
+                onChange={e => setInput(e.target.value)}
+            />
+            <p>{convert(input, data.amount)} {data.base}</p>
         </div>
     );
   };
